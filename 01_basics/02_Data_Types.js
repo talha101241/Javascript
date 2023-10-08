0a303c3b7b5ab6ba34050524f152c418ce5bbf35
talha101241 <65253444+talha101241@users.noreply.github.com>
2023-10-08 17:51:44 +0000

    Data Types

---
+++ 01_basics/02_Data_Types.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert(3+3); // we are using nodeJs, not browser
+
+let name = "Watson";
+let age = 18;
+let IsLoggedIn = false;
+
+// number => to pwr 53
+// bigint
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output undefine
+console.log(typeof null); // output Object
